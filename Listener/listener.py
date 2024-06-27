40ec093941df7efde83cbb3d681665d522833314
khan-abdullah0416 <khan.abdullah0416@gmail.com>
2024-06-27 13:21:01 +0100

    Created listener

--- Listener/listener.py
+++ Listener/listener.py
@@ -1,13 +1,25 @@
+from packets import packets
 import socket
 
-udp_ip = '127.0.0.1'
-udp_port = 5005
 
-# AF_INET is Internet
-# SOCK_DGRAM is UDP
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class Listener:
+    def __init__(self, ip_address, port) -> None:
+        self.ip_address = ip_address
+        self.port = port
 
-sock.bind((udp_ip, udp_port))
+        # AF_INET is Internet
+        # SOCK_DGRAM is UDP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.ip_address, self.port))
 
-while True:
-    data, addr = sock.recvfrom(1024) # Buffer size of 1024 bytes
+
+    def listener(self):
+        # Receive the packet
+        packet = self.socket.recv(2048)
+
+        # Create PacketHeader class
+        header = packets.PacketHeader.from_buffer_copy(packet)
+
+        # If the packet_id is 6, then create a PacketCarTelemetryData class
+        if header.packet_id == 6:
+            return packets.PacketCarTelemetryData.from_buffer_copy(packet)
